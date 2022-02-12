67ed14991bdfb6e483e80036e902db942aaacfb2
RoiGaon <roigaon@gmail.com>
2022-02-12 14:17:37 +0200

    handle useAddItem fetcher output

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,35 +1,50 @@
 import { useAddItem } from "@common/cart";
+import { Cart } from "@common/types/cart";
 import { MutationHook } from "@common/types/hooks";
-import { getCheckoutId } from "@framework/utils";
+import { CheckoutLineItemsAddPayload } from "@framework/schema";
+import { checkoutToCart, getCheckoutId } from "@framework/utils";
 import { checkoutLineItemsAddMutation } from "@framework/utils/mutations";
 
 export default useAddItem;
 
-export const handler: MutationHook = {
+export type AddItemHookDescriptor = {
+  fetcherInput: {
+    variantId: string;
+    quantity: number;
+  };
+  fetcherOutput: {
+    checkoutLineItemsAdd: CheckoutLineItemsAddPayload;
+  };
+  data: Cart;
+};
+
+export const handler: MutationHook<AddItemHookDescriptor> = {
   fetcherOptions: {
     query: checkoutLineItemsAddMutation,
   },
   fetcher: async ({ fetch, options, input }) => {
-    const response = await fetch({
+    const variables = {
+      checkoutId: getCheckoutId(),
+      lineItems: [
+        {
+          variantId: input.variantId,
+          quantity: 1,
+        },
+      ],
+    };
+    const { data } = await fetch({
       ...options,
-      variables: {
-        checkoutId: getCheckoutId(),
-        lineItems: [
-          {
-            variantId: input.variantId,
-            quantity: 1,
-          },
-        ],
-      },
+      variables,
     });
-    return response;
+
+    const cart = checkoutToCart(data.checkoutLineItemsAdd.checkout);
+
+    return cart;
   },
   useHook: ({ fetch }) => {
-    return async (input: any) => {
+    return async (input) => {
       const response = await fetch(input);
-      return {
-        output: response,
-      };
+      return response;
     };
   },
 };
