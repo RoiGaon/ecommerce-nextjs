a1358308937eb4098eebf45f1b69511c7120997c
RoiGaon <roigaon@gmail.com>
2022-02-25 16:30:03 +0200

    start to implement use remove item

---
+++ framework/shopify/cart/use-remove-item.ts
@@ -0,0 +1,22 @@
+import useRemoveItem from "@common/cart/use-remove-item";
+
+export default useRemoveItem;
+
+export const handler = {
+  fetcherOptions: {
+    query: "query { hello }",
+  },
+  async fetcher({ input, options, fetch }: any) {
+    const { data } = await fetch({ ...options });
+
+    return data + "_Modified!!!";
+  },
+  useHook:
+    ({ fetch }: any) =>
+    () => {
+      return async (input: any) => {
+        const data = await fetch(input);
+        return data;
+      };
+    },
+};
